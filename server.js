cb94b9eed77e2fad1ebb2782f818ea00685b2452
Jbolts <77908382+Jbolton89@users.noreply.github.com>
2021-09-04 14:47:07 +1000

    minor fix

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const path = require('path'); 
 
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 
 require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
 
-const PORT = process.env.PORT || 8080;
+
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
