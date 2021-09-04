53854ebfb755168ab6c9eef1e64c0ffc723ddb2b
Jbolts <77908382+Jbolton89@users.noreply.github.com>
2021-09-04 14:45:15 +1000

    add gitignore

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path'); 
+
+const app = express();
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public"));
+
+
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
